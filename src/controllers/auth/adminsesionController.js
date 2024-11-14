4d87da0433d0b1cb779403ec78c0e614a5330195
bancodeherramientassenaciaa <bancodeherramientassenaciaa@gmail.com>
2024-11-14 12:53:20 -0500

    organizar horas

--- src/controllers/auth/adminsesionController.js
+++ src/controllers/auth/adminsesionController.js
@@ -26,7 +26,7 @@ const formatFecha = (fecha, ajusteHoras = 0) => {
 
 const ajustarHora = (date) => {
   const offset = 0; // Esto es para cuadrar las horas manualmente porque en la bd se están guardando las hroas con un desface de 5 horas
-  const adjustedDate = new Date(date.getTime() + offset * 60 * 60 * 1000);
+  const adjustedDate = new Date(date.getTime() + 60 * 60 * 1000);
   return format(adjustedDate, 'yyyy-MM-dd HH:mm:ss', { locale: es });
 };
 
